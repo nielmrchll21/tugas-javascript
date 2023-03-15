6c6c9a4bdf8e9710a8fef5fd8d54404e80d1a43f
marchella vidya <124251906+nielmrchll21@users.noreply.github.com>
2023-03-16 00:49:36 +0800

    upload file js

---
+++ js/kalkulator.js
@@ -0,0 +1,43 @@
+function tambah(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 + a2;
+    frm.hasil.value = total;
+}
+
+function kurang(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 - a2;
+    frm.hasil.value = total;
+}
+function bagi(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 / a2;
+    frm.hasil.value = total;
+}
+function kali(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 * a2;
+    frm.hasil.value = total;
+}
+function pangkat(){
+    var frm = document.getElementById('kalkulator');
+    var a1 = parseFloat(frm.angka.value);
+    var a2 = parseFloat(frm.angka2.value);
+    var total = a1 ** a2;
+    frm.hasil.value = total;
+}
+function peringatan(){
+    let peringatan = document.getElementById('kalkulator').angka.angka2.value;
+    let input ='proses perhitungan anda selesai ' + peringatan ;
+    let no_input = 'angka belum terisi';
+    let hasil = (peringatan != '')? input : no_input;
+    document.getElementById('hasil').innerHTML = hasil;
+}
